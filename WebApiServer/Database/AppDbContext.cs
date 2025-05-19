e5697409b7320578b3d3183bb64a0c8c281e24e6
Pawesh Bhaggan <parweshbhaggan@outlook.com>
2025-05-19 17:09:50 +0200

    Created Product model

--- WebApiServer/Database/AppDbContext.cs
+++ WebApiServer/Database/AppDbContext.cs
@@ -1,6 +1,32 @@
-﻿namespace WebApiServer.Database
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiServer.Models;
+
+namespace WebApiServer.Database
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        private readonly string _databaseName = "PoS_System";
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Database arugments
+            string host = "localhost";
+            string port = "5432";
+            string database = "postgres";
+            string username = "postgres";
+            string password = "Parwesh";
+            string connectionBuilder = $"Host={host};Port={port};Database={database};Username={username};Password={password};";
+
+
+            optionsBuilder.UseNpgsql(connectionBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema(_databaseName);
+        }
+
     }
 }
--- WebApiServer/Models/Product.cs
+++ WebApiServer/Models/Product.cs
@@ -2,5 +2,20 @@
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public decimal Cost { get; set; }
+        public decimal SalePriceBeforeTax { get; set; }
+        public decimal Tax { get; set; }
+        public decimal SalePrice { get; set; } //calculated with tax
+        public int UnitOfMeasure { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int Quantity { get; set; }
+
     }
+
+  
 }
